9779d781c909f8897b1a3c1d9cea982dd77d7ff5
ferhad <linx76344@gmail.com>
2025-10-28 18:36:06 +0400

    Refactor employee classes and add new functionality

--- Program.cs
+++ Program.cs
@@ -1,118 +1,157 @@
-﻿using Interfaces;
+using Interfaces;
 using System.Linq;
 class Program
 {
-    class Dog : IAnimal
-    {
-        public void MakeSound()
-        {
-            Console.WriteLine("Hav-hav");
-        }
-
-        public void Move()
-        {
-            Console.WriteLine("Dog runs");
-        }
-    }
-
-    class Bird : IAnimal
-    {
-        public void MakeSound()
-        {
-            Console.WriteLine("Cik-cik");
-        }
-
-        public void Move()
-        {
-            Console.WriteLine("Bird flies");
-        }
-    }
-
-    class Car : IVehicle
-    {
-        public void Start()
-        {
-            Console.WriteLine("Car startted");
-        }
-
-        public void Stop()
-        {
-            Console.WriteLine("Car stopped");
-        }
-    }
-
-    class Employee
+    abstract class Employee
     {
         public string Name { get; set; }
+        public string Position { get; set; }
+        public decimal BaseSalary { get; set; }
 
-        private string surname;
-        public string Surname
-        {
-            get
-            {
-                return surname;
-            }
-            set
-            {
-                if (string.IsNullOrEmpty(value))
-                {
-                    Console.WriteLine("error");
-                    return;
-                }
-                if (!char.IsUpper(value[0]) || !value.All(char.IsLetter))
-                {
-                    Console.WriteLine("error");
-                    return;
-                }
-                surname = value;
-            }
-        }
-
-        public Employee(string name, string surname, int salary)
+        public Employee(string name, string position, decimal baseSalary)
         {
             Name = name;
-            Surname = surname;
+            Position = position;
+            BaseSalary = baseSalary;
         }
-    }
 
-    class Department
+        public abstract decimal CalculateSalary();
+        public abstract void DisplayInfo();
+
+        public virtual decimal GiveBonus(decimal amount)
+        {
+            return BaseSalary += amount;
+            
+        }
+}
+
+    class Developer : Employee,IWorkable,IReportable
     {
-        public string name;
-        private int EmployeeLimit;
-        private int SalaryLimit;
-        private Employee[] Employees = new Employee[10];
-        private int salary;
-        public int Salary 
+        public int ProjectCount { get; set; }
+
+        public override decimal CalculateSalary()
         {
-            get
-            {
-                return salary;
-            }
-            set
-            {
-                if(value < 250)
-                {
-                    Console.WriteLine("Salary 250 boyuk olmalidir");
-                    return;
-                }
-                salary = value;
-            }
+            return BaseSalary + (ProjectCount * 200);
+;        }
+        public Developer(string name, decimal baseSalary, int projectCount)
+        : base(name, "Developer", baseSalary)
+        {
+            this.ProjectCount = projectCount;
         }
-        private int employeeCount = 0;
-        internal void AddEmployee(Employee employee)
+
+        public void Work()
         {
-            Employees[employeeCount] = employee;
-            employeeCount++;
+            Console.WriteLine();
+        }
+
+        public void GenerateReport(string str)
+        {
+            Console.WriteLine();
+        }
+
+        public void ShowReport()
+        {
+            Console.WriteLine();
+        }
+
+        public override void DisplayInfo()
+        {
+            
         }
 
     }
 
+    class Manager : Employee, IWorkable, IReportable
+    {
+        public Manager(string name, decimal baseSalary, int teamSize)
+        : base(name, "Manager", baseSalary)
+        {
+            this.TeamSize = teamSize;
+        }
+
+        public int TeamSize { get; set; }
+        public override decimal CalculateSalary()
+        {
+            return BaseSalary + (TeamSize * 200);
+            ;
+        }
+
+        public void Work()
+        {
+            Console.WriteLine();
+        }
+
+        public void GenerateReport(string str)
+        {
+            Console.WriteLine(str);
+        }
+
+        public void ShowReport()
+        {
+            Console.WriteLine();
+        }
+
+        public override void DisplayInfo()
+        {
+
+        }
+    }
+
+    class Intern : Employee, IWorkable, IReportable
+    {
+
+        public Intern(string name, decimal baseSalary)
+        : base(name, "Intern", baseSalary)
+        {
+        }
+        public override decimal CalculateSalary()
+        {
+            return BaseSalary;
+        }
+        public void Work()
+        {
+            Console.WriteLine();
+        }
+
+        public void GenerateReport(string str)
+        {
+            Console.WriteLine(str);
+        }
+
+        public void ShowReport()
+        {
+            Console.WriteLine();
+        }
+
+        public override void DisplayInfo()
+        {
+            
+        }
+    }
 
     static void Main(string[] args)
     {
-        
+        Developer developer_1 = new Developer("Namiq",4600,3);
+        Manager manager_1 = new Manager("Akif", 5750, 2);
+        Intern inter_1 = new Intern("Ali", 800);
+
+        developer_1.GenerateReport("Completed 3 new modules");
+        developer_1.Work();
+        developer_1.CalculateSalary();
+        developer_1.GiveBonus(3);
+
+        manager_1.GenerateReport("eam performance improved by 20%");
+        manager_1.Work();
+        manager_1.CalculateSalary();
+        manager_1.GiveBonus(3);
+
+        inter_1.GenerateReport("Learned basics of C#");
+        inter_1.Work();
+        inter_1.CalculateSalary();
+        inter_1.GiveBonus(3);
     }
 }
+
 
 
 
